70d8436a04042cef2084bbacc6247cfa1b708f52
MichaelRay06 <michaelo.ray06@gmail.com>
2022-11-30 09:31:28 +0000

    updated with 3 dockers runners

---
+++ cypress/integration/login/mutiple.steps.js
@@ -0,0 +1,42 @@
+import { Given, When, And, Then } from 'cypress-cucumber-preprocessor/steps';
+
+import  MenuPage from '../../support/pageObjects/menuPage' 
+import TopMenuPage from '../../support/pageObjects/topMenuPage'
+
+
+Given("I am on test automationstore", () => {
+    cy.navigateTo_Url();
+    cy.clearCookies_LocalStoreage();
+  });
+
+
+When("I mouse over items {string} and click on {string}", (menu, subMenu)=> {
+    MenuPage.getMenuItems(menu,subMenu )
+
+});
+
+And("I to make mutiple items {string}",(selectedItems)=>{
+    MenuPage.selectItems(selectedItems)
+
+
+});
+
+And("click on menu cart button",()=>{
+    MenuPage.menu_cartButton();
+
+
+});
+
+And("I update the cart table",()=>{
+    TopMenuPage.updateChatTable();
+
+    
+});
+
+Then("I could verify the total amount paid",()=>{
+    TopMenuPage.tableTotalRow();
+  
+
+    
+});
+
